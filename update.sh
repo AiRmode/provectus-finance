4f37de36d02b632bcace308b8f46d2f409a820e5
Alexey Shevchuk <ashevchuk@determine.com>
2017-04-11 22:32:37 +0300

    Kill both process when restart... so far just a workaround

--- update.sh
+++ update.sh
@@ -11,9 +11,11 @@ mvn clean test install package
 cd tax-management-spring-boot/target
 
 pid=$(ps aux | grep $server_port | awk 'FNR == 1{print $2}')
+echo "killing tomcat PID: $pid"
+kill -9 $pid
 
-echo "tomcat PID: $pid"
-
+pid=$(ps aux | grep $server_port | awk 'FNR == 1{print $1}')
+echo "killing tomcat PID: $pid"
 kill -9 $pid
 
 nohup java -jar tax-management-spring-boot-1.0-SNAPSHOT.jar --server.port=$server_port &
