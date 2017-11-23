d9a312e862960361638c955855f7ea340321412f
agricenko <agricenko@determine.com>
2017-11-23 23:45:38 +0200

    Xls Payment Util. Fixed privatbank xls document parsing.

--- tax-management-core/src/main/java/com/provectus/taxmanagement/util/PaymentUtil.java
+++ tax-management-core/src/main/java/com/provectus/taxmanagement/util/PaymentUtil.java
@@ -5,11 +5,17 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.IllegalFormatException;
 import java.util.List;
 
@@ -19,22 +25,32 @@ import java.util.List;
 @Component
 public class PaymentUtil {
 
-    public void parseDocument(File document) throws IOException {
-        Document doc = Jsoup.parse(document, "UTF-8");
+    public static final String DATE_FORMAT = "dd.MM.YYYY HH:mm:ss";
 
-        Elements tables = doc.getElementsByTag("table");
+    private static final Logger logger = LoggerFactory.getLogger(PaymentUtil.class);
 
-        if (tables.size() != 2) System.out.println("Table counts is not correct");
+    public List<TaxRecord> parseDocument(File document) throws IOException, ParseException {
+        try {
+            Document doc = Jsoup.parse(document, "UTF-8");
 
-        String headerText = parseHeader(tables.get(0));
-        parseTable(tables.get(1));
+            Elements tables = doc.getElementsByTag("table");
+
+            if (tables.size() != 2) System.out.println("Table counts is not correct");
+
+            String headerText = parseHeader(tables.get(0));
+            return parseTable(tables.get(1));
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+            //TODO wrap exception
+            throw e;
+        }
     }
 
     private String parseHeader(Element element) {
         return element.text();
     }
 
-    private List<TaxRecord> parseTable(Element element) {
+    private List<TaxRecord> parseTable(Element element) throws ParseException {
         List<TaxRecord> taxRecords = new ArrayList<>();
         
         Elements trs = element.getElementsByTag("tr");
@@ -43,12 +59,13 @@ public class PaymentUtil {
             Elements tDs = row.getElementsByTag("td");
 
             TaxRecord taxRecord = parseTaxRecordRaw(tDs);
+            taxRecords.add(taxRecord);
         }
 
         return taxRecords;
     }
 
-    private TaxRecord parseTaxRecordRaw(Elements tDs) {
+    private TaxRecord parseTaxRecordRaw(Elements tDs) throws ParseException {
         TaxRecord taxRecord = new TaxRecord();
 
         String number = tDs.get(0).text();
@@ -63,6 +80,21 @@ public class PaymentUtil {
         String bill = tDs.get(9).text();
         String MFO = tDs.get(10).text();
         String reference = tDs.get(11).text();
+
+        if (currency.equalsIgnoreCase("UAH")) {
+            taxRecord.setUahRevenue(incomeAmount + consumptionAmount);
+        } else if (currency.equalsIgnoreCase("USD")) {
+            taxRecord.setUsdRevenue(incomeAmount + consumptionAmount);
+        }
+
+        try {
+            Date receivingDate = new SimpleDateFormat(DATE_FORMAT).parse(receivingDay + " " + receivingTime);
+            taxRecord.setReceivingDate(receivingDate);
+        } catch (ParseException e) {
+            logger.error(e.getMessage(), e);
+            //TODO wrap exception
+            throw e;
+        }
 
         return taxRecord;
     }
--- tax-management-core/src/test/java/com/provectus/taxmanagement/util/PaymentUtilTest.java
+++ tax-management-core/src/test/java/com/provectus/taxmanagement/util/PaymentUtilTest.java
@@ -1,10 +1,19 @@
 package com.provectus.taxmanagement.util;
 
+import com.provectus.taxmanagement.entity.TaxRecord;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by agricenko on 10.09.2017.
@@ -13,9 +22,43 @@ public class PaymentUtilTest {
 
     URL resource = Thread.currentThread().getContextClassLoader().getResource("statements.xls");
 
+    public List<TaxRecord> getStubRecords() throws ParseException {
+        List<TaxRecord> taxRecords = new ArrayList<>();
+
+        DateFormat dateFormat = new SimpleDateFormat(PaymentUtil.DATE_FORMAT);
+
+        TaxRecord taxRecord = new TaxRecord();
+        taxRecord.setUahRevenue(-30.0);
+        taxRecord.setReceivingDate(dateFormat.parse("01.09.2017 15:47:00"));
+        taxRecords.add(taxRecord);
+
+        taxRecord = new TaxRecord();
+        taxRecord.setUahRevenue(-10.50);
+        taxRecord.setReceivingDate(dateFormat.parse("22.08.2017 11:19:00"));
+        taxRecords.add(taxRecord);
+
+        taxRecord = new TaxRecord();
+        taxRecord.setUahRevenue(9000.50);
+        taxRecord.setReceivingDate(dateFormat.parse("21.08.2017 12:25:00"));
+        taxRecords.add(taxRecord);
+
+        taxRecord = new TaxRecord();
+        taxRecord.setUahRevenue(-4000.00);
+        taxRecord.setReceivingDate(dateFormat.parse("18.08.2017 10:04:00"));
+        taxRecords.add(taxRecord);
+
+        taxRecord = new TaxRecord();
+        taxRecord.setUahRevenue(255600.00);
+        taxRecord.setReceivingDate(dateFormat.parse("15.08.2017 10:42:00"));
+        taxRecords.add(taxRecord);
+
+        return taxRecords;
+    }
+
     @Test
-    public void testParseDocument() throws IOException {
+    public void testParseDocument() throws IOException, ParseException {
         PaymentUtil paymentUtil = new PaymentUtil();
-        paymentUtil.parseDocument(new File(resource.getPath()));
+        List<TaxRecord> taxRecords = paymentUtil.parseDocument(new File(resource.getPath()));
+        assertEquals(taxRecords, getStubRecords());
     }
 }
